c58f8339316269735a7df0051ad289994db60f42
Mariah Wear <wearmariahk@gmail.com>
2023-03-14 15:11:09 -0600

    adding routes for each action

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,56 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Tag.findAll({
+      include: [{model: Tag, through: ProductTag}] ,
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{model: Product}, {model: Tag}],
+    });
+    if (!productData) {
+      res.status(404).json({message: 'No product found with that id!'});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err){
+    res.status(500).json(err);
+  }
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const productData = await Product.update(req.body, 
+      { where: {
+        product_name: req.body.product_name,
+        price: req.body.price,
+        stock: req.body.stock,
+        tagIds: req.body.tagIds,
+      },
+    });
+    if (!productData[0]) {
+      res.status(404).json({message: 'No product with this id!'});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
   /* req.body should look like this...
     {
       product_name: "Basketball",
