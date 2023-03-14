4f99d1a40f4cc54456f60baf23b3a05306796e3f
Mariah Wear <wearmariahk@gmail.com>
2023-03-14 12:28:00 -0600

    defining models and relationships

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,27 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // define columns - create an ID, another one that's a product_id that's a foreign key, tag_id that's a foreign key. 
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      }
+    }
   },
   {
     sequelize,
